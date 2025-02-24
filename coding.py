ffe219b382367b5d50954d172784b73eef815d09
danillipski <183762808+danillipski@users.noreply.github.com>
2025-02-24 18:26:08 +0100

    Test file operations and Test FastAPI

---
+++ coding.py
@@ -0,0 +1,27 @@
+# A TO DO TASK FROM MY COMMAND LINE
+# IT WILL SAFE TEXT TO THE FILE, MODIFY IT AND OTHER THINGS
+
+import os
+
+is_running = True
+file_path = "notes.txt"
+while is_running:
+        
+    def new_task():
+        task = input("What is my to do task? / q to Quit :").lower()
+        if task == "q".lower():
+            is_running = False
+            print(task)
+
+    def write_to_file():
+        with open(file=file_path, mode="w") as file:
+            file.write(task)
+            print(f"{task} has been added to {file}")
+
+    def read_file():
+        with open(file=file_path, mode="r") as file:
+            pass
+
+    new_task()
+    next_action = input("what to do next?")
+    #if next_action == 
